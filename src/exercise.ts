d40f4fff1d51c795113b3a925f076b0c21a2b203
Bruno Brigido Vilanova <brunobrigidovilanova@gmail.com>
2023-06-27 15:37:59 -0300

    First commit

---
+++ src/exercise.ts
@@ -0,0 +1,10 @@
+function multiplica(numero1: number, numero2: number): number {
+    return numero1 * numero2;
+}
+
+function dizOla(nome: string): string  {
+    return `Olá ${nome}`;
+}
+
+console.log(multiplica(5, 25))
+console.log(dizOla('Bruno'))
